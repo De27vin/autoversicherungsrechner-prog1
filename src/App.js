16177c2ce3023536d182a247b76bc784a0df2fc8
Devin de Spindler <devin.vipxxx@gmail.com>
2024-05-31 19:03:55 +0200

    Long picture, Progress-bar & a prototype for choosing car brand/model

--- src/App.js
+++ src/App.js
@@ -3,15 +3,49 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import logo from './assets/logo.png'
+import logo from './assets/logo.png';
+import Image from 'react-bootstrap/Image';
+import ImageChooseCar from './assets/chooseCar.png';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+const carModels = {
+  Audi: ['A3', 'A4', 'A6'],
+  BMW: ['M3', 'M4', 'M5'],
+  VW: ['Polo', 'Golf', 'Up'],
+  Toyota: ['Corolla', 'Camry', 'Yaris'],
+  Honda: ['Civic', 'Accord', 'Jazz'],
+  Nissan: ['Altima', 'Sentra', 'Versa'],
+  Ford: ['Focus', 'Fusion', 'Mustang'],
+  Hyundai: ['Elantra', 'Sonata', 'Accent'],
+  Kia: ['Optima', 'Sorento', 'Rio'],
+  Mazda: ['MX-5', '6', 'CX-5']
+};
+
+function BigImage() {
+  return (
+    <div className="d-flex justify-content-center">
+      <Image src={ImageChooseCar} fluid className='big-image'/>
+    </div>
+  );
+}
+
+
 function App() {
   const [show, setShow] = useState(false);
+  const [selectedBrand, setSelectedBrand] = useState('');
+  const [models, setModels] = useState([]);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const handleBrandChange = (event) => {
+    const brand = event.target.value;
+    setSelectedBrand(brand);
+    setModels(carModels[brand] || []);
+  };
 
   return (
     <div className="App">
@@ -32,6 +66,33 @@ function App() {
         </Navbar>
 
         <div className="custom-divider my-3 mx-auto"></div>
+
+        <BigImage/>
+
+        <div className='d-flex justify-content-center'>
+          <ProgressBar now={20} className='progress-bar'/> {/* Funktioniert noch nicht! label={`${now}%`} */}
+        </div>
+
+        <div className="container">
+          <div className="form-group mt-3">
+            <select id="brandSelect" className="form-control" onChange={handleBrandChange}>
+              <option value="">Wähle eine Automarke</option>
+              {Object.keys(carModels).map((brand) => (
+                <option key={brand} value={brand}>{brand}</option>
+              ))}
+            </select>
+          </div>
+          {selectedBrand && (
+            <div className="form-group mt-3">
+              <select id="modelSelect" className="form-control">
+                <option value="">Wähle ein Automodell</option>
+                {models.map((model) => (
+                  <option key={model} value={model}>{model}</option>
+                ))}
+              </select>
+            </div>
+          )}
+        </div>
 
         <Offcanvas show={show} onHide={handleClose} placement="end">
           <Offcanvas.Header closeButton>
